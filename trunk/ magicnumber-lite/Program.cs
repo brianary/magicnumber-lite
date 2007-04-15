d3b6bb8696cfdf80703d792ab0fcc90b292c61be
brianary <brianary@61501124-1f2e-0410-9f08-b51233da51be>
2007-04-15 05:50:36 +0000

    Minor cleanup.

--- trunk/ magicnumber-lite/Program.cs
+++ trunk/ magicnumber-lite/Program.cs
@@ -6,17 +6,17 @@ using MagicNumberLite;
 
 namespace MagicNumberLite
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Inspector inspector = new Inspector();
-            foreach (string arg in args)
-            {
-                DataType type = inspector.GetDataType(new StreamReader(arg).BaseStream);
-                if (type == null) Console.WriteLine("{0} not identified", arg);
-                else Console.WriteLine(String.Join("\t", new string[] { arg, type.Name, type.MimeType, type.Extension }));
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Inspector inspector = new Inspector();
+			foreach (string arg in args)
+			{
+				DataType type = inspector.GetDataType(new StreamReader(arg).BaseStream);
+				if (type == null) Console.WriteLine("{0} not identified", arg);
+				else Console.WriteLine(String.Join("\t", new string[] { arg, type.Name, type.MimeType, type.Extension }));
+			}
+		}
+	}
 }
