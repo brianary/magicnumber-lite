393dea731f11ad31a3bea4b184db66dc002ed668
brianary <brianary@61501124-1f2e-0410-9f08-b51233da51be>
2007-04-17 04:05:26 +0000

    Version 1.2

---
+++ tags/release-1.2/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MagicNumberLite;
+
+namespace MagicNumberLite
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Inspector inspector = new Inspector();
+			foreach (string arg in args)
+			{
+				DataType type = inspector.GetDataType(new StreamReader(arg).BaseStream);
+				if (type == null) Console.WriteLine("{0} not identified", arg);
+				else Console.WriteLine(String.Join("\t", new string[] { arg, type.Name, type.MimeType, type.Extension }));
+			}
+		}
+	}
+}
